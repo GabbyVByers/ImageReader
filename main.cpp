920e2bae9e22b504d8ba7033ae23b4acd39107a1
Gabby <GabbyVByers@gmail.com>
2025-10-17 00:38:25 -0700

    stb_image

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 
-
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include <iostream>
 
 int main() {
+
+	int width, height, channels;
+	unsigned char* data = stbi_load("image.png", &width, &height, &channels, 0);
+
 	std::cout << "Hello World\n";
 	return 0;
 }
+
